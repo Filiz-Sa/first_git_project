fe2790284684c03eef50d370fef8a0c285291c6c
Filiz-Sa <filizaydiniz@hotmail.com>
2025-02-26 16:27:46 +0300

    HelloWorld Uptated

--- src/day01/HelloWordl.java
+++ src/day01/HelloWordl.java
@@ -1,6 +1,10 @@
 package day01;
 
 public class HelloWordl {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+    }
 /*
 class ismi kırmızı=file is added to the working directory but it has not been saved in local  repository.
 gitignore yellow color( bende kırmızı)
