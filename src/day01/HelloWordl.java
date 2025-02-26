0b00d890f4f65b5dde4fdb1a0a53de0092c8893d
Filiz-Sa <filizaydiniz@hotmail.com>
2025-02-26 16:03:46 +0300

    HelloWorld Class

---
+++ src/day01/HelloWordl.java
@@ -0,0 +1,9 @@
+package day01;
+
+public class HelloWordl {
+/*
+class ismi kırmızı=file is added to the working directory but it has not been saved in local  repository.
+gitignore yellow color( bende kırmızı)
+ class ismine sağ tıkla git ->add to gitignore->git ignore de sarı renk olacak
+ */
+}
